e292222fa55084b00c9d6b4f1d0f04accddd3068
nxsaloj <niisaloj@gmail.com>
2024-10-04 12:55:00 -0600

    Configure app to work with vuetify and pinia

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import { createPinia } from 'pinia'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const pinia = createPinia()
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+createApp(App)
+  .use(vuetify)
+  .use(store)
+  .use(pinia)
+  .use(router).mount('#app')
