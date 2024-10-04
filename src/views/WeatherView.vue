91246c63307d81429b032384c6a7d4fb116ab66c
nxsaloj <niisaloj@gmail.com>
2024-10-04 12:56:48 -0600

    Adding Weather view component

---
+++ src/views/WeatherView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Nombre de la ciudad</h1>
+    <CityAutocomplete v-model="defaultCity"></CityAutocomplete>
+    <div v-if="defaultCity">
+      <h1>Resumen del clima: </h1>
+      <WeatherComponent :City="defaultCity"></WeatherComponent>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { City } from '../components/city/type/City'
+
+import CityAutocomplete from '@/components/CityAutocomplete/CityAutocomplete.vue'
+import WeatherComponent from '@/components/WeatherComponent/WeatherComponent.vue'
+
+const defaultCity = ref<City|null>(null)
+</script>
