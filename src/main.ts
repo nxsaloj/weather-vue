2d707199a2d8e355c7f14becb3aafd6f226ddc4d
Nelson Saloj <niisaloj@gmail.com>
2024-10-04 14:00:36 -0600

    Configuring icons for vuetify

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,23 @@ import store from './store'
 import { createPinia } from 'pinia'
 
 // Vuetify
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 const pinia = createPinia()
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  },
   components,
   directives
 })
